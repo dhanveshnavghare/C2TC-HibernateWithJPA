cdb58d951ba74030837b44f9e595303839281b5e
NAMRATA NAVGHARE <NAMRATA NAVGHARE@DESKTOP-5NJ6TES>
2022-07-15 00:18:45 +0530

    table per class

---
+++ JPATableperclass/src/com/tns/entities/InheritanceTest.java
@@ -0,0 +1,34 @@
+package com.tns.entities;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class InheritanceTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em = factory.createEntityManager();
+		em.getTransaction().begin();
+		
+		Employee employee = new Employee();  // object : employee
+		employee.setName("Dhanvesh");
+		employee.setSalary(1243);	
+		em.persist(employee); 
+	
+		Manager manager = new Manager();
+		manager.setName("Navghare");
+		manager.setSalary(2223);
+		manager.setDepartmentName("IT");
+		em.persist(manager);   // insert
+		em.getTransaction().commit();
+		
+		em.close();
+		factory.close();
+		System.out.println("done");		
+	}
+
+}
